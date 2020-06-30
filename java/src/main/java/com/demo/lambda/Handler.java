a098fe0b7af1c5299c9af510a89ace33d5f10b75
Christian Tschenett <developer@furthermore.ch>
2020-06-30 21:22:08 +0200

    logging added

--- java/src/main/java/com/demo/lambda/Handler.java
+++ java/src/main/java/com/demo/lambda/Handler.java
@@ -1,20 +1,23 @@
 package com.demo.lambda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-//public class Handler implements RequestHandler<Map<String, String>, String> {
 public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+	private static final Logger logger = LoggerFactory.getLogger(Handler.class);
+	
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		logger.info("Hello {}", "world");
+		
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
 		response.setStatusCode(200);
 		response.setBody("hello world");
+		
 		return response;
 	}
-	
-//	public String handleRequest(Map<String, String> input, Context context) {
-//		return "hello world";
-//	}
 }
